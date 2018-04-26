aa4b57a2f5b0a0b175511eb871b87e6ed02975d0
Kata <2726456039@qq.com>
2018-04-26 14:41:07 +0800

    modified:   src/main/java/practice1/Security.java

--- src/main/java/practice1/Security.java
+++ src/main/java/practice1/Security.java
@@ -11,28 +11,9 @@ public class Security {
     }
 
     public boolean hasAccess(User user, Permission permission, ImmutableList<Permission> permissions) {
-
-        boolean isAccess = false;
-        if (user == null) {
-            return isAccess;
-        }
-
-        if (permission == null) {
-            return isAccess;
-        }
-
-        if (permissions.size() == 0) {
-            return isAccess;
-        }
-
-        if (securityChecker.isAdmin()) {
-            isAccess = true;
-        }
-
-        if (this.securityChecker.checkPermission(user, permission) || permissions.contains(permission)) {
-            isAccess = true;
-        }
-
-        return isAccess;
+        boolean isAdmin = securityChecker.isAdmin();
+        boolean isPermission = securityChecker.checkPermission(user, permission);
+        boolean containsPermission = permissions.contains(permission);
+        return isAdmin || isPermission || containsPermission;
     }
 }
